b410c99e9d93bb4a0b403cf77251201ff0a78d7a
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-24 16:37:27 +0000

    provo nav boostrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,48 +1,5 @@
 <template>
   <div class="header">
-    <div class="logo_container">
-      <img class="logo" src="../assets/images/logo.png" alt="" />
-    </div>
-    <div class="nav">
-      <ul>
-        <li>
-          <img class="header_icons" src="../assets/images/home.png" alt="" />
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <img
-            class="header_icons"
-            src="../assets/images/document.png"
-            alt=""
-          />
-          <a href="#">Pages</a>
-        </li>
-        <li>
-          <img class="header_icons" src="../assets/images/printer.png" alt="" />
-          <a href="#">Blog</a>
-        </li>
-        <li>
-          <img class="header_icons" src="../assets/images/cart.png" alt="" />
-          <a href="#">Shop</a>
-        </li>
-        <li>
-          <img class="header_icons" src="../assets/images/lab.png" alt="" />
-          <a href="#">Shortcodes</a>
-        </li>
-        <li>
-          <img class="header_icons" src="../assets/images/chat.png" alt="" />
-          <a href="#">Support</a>
-        </li>
-        <li>
-          <img
-            class="header_icons"
-            src="../assets/images/envelope.png"
-            alt=""
-          />
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
   </div>
 </template>
 
