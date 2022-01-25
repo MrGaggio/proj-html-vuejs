669d0d3bc18b0c272f311656caa677aef159404b
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-25 01:17:26 +0000

    ritagliate icone e sistemate

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,60 @@
 <template>
   <div class="header">
+    <nav class="navbar navbar-light">
+      <div class="container header_container">
+        <a class="navbar-brand" href="#">
+          <img
+            class="header_icons"
+            src="../assets/images/logo.png"
+            alt=""
+            width="200"
+            height=""
+          />
+        </a>
+        <ul class="nav justify-content-center text-center">
+          <li class="nav-item">
+            <img class="header_icons" src="../assets/images/home.png" alt="" />
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <img
+              class="header_icons"
+              src="../assets/images/document.png"
+              alt=""
+            />
+            <a class="nav-link" href="#">Pages</a>
+          </li>
+          <li class="nav-item">
+            <img
+              class="header_icons"
+              src="../assets/images/printer.png"
+              alt=""
+            />
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <img class="header_icons" src="../assets/images/cart.png" alt="" />
+            <a class="nav-link" href="#">Shop</a>
+          </li>
+          <li class="nav-item">
+            <img class="header_icons" src="../assets/images/lab.png" alt="" />
+            <a class="nav-link" href="#">Shortcodes</a>
+          </li>
+          <li class="nav-item">
+            <img class="header_icons" src="../assets/images/chat.png" alt="" />
+            <a class="nav-link" href="#">Support</a>
+          </li>
+          <li class="nav-item">
+            <img
+              class="header_icons"
+              src="../assets/images/envelope.png"
+              alt=""
+            />
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -15,37 +70,56 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-.header {
-  margin: 0 200px;
-  display: flex;
-  height: 150px;
-  align-items: center;
-  justify-content: center;
-  background-color: #fefefe;
+.container {
+  padding: 0;
 }
-.logo {
-  width: 200px;
+li.nav-item {
+  margin: 0 20px;
+  
+}
+li.nav-item:hover{
+  background-color: #FD6500;
 }
 
-ul {
-  list-style-type: none;
-  display: flex;
-  align-self: center;
-  text-align: center;
-}
-li {
-  margin: 0 3.5em;
-}
-li:hover {
-  cursor: pointer;
-}
-.header_icons {
-  width: 40px;
+img.header_icons {
+  color: #56509f;
+  font-weight: 700;
 }
 a {
-  text-decoration: none !important;
   color: #56509f !important;
-  text-align: center;
-  display: block;
 }
+
+// .header {
+//   margin: 0 200px;
+//   display: flex;
+//   height: 150px;
+//   align-items: center;
+//   justify-content: center;
+//   background-color: #fefefe;
+// }
+// .logo {
+//   width: 200px;
+// }
+
+// ul {
+//   list-style-type: none;
+//   display: flex;
+//   align-self: center;
+//   text-align: center;
+// }
+// li {
+//   margin: 0 3.5em;
+// }
+// li:hover {
+//   cursor: pointer;
+// }
+// .header_icons {
+//   width: 40px;
+// }
+// a {
+//   text-decoration: none !important;
+//   color: #56509f !important;
+//   text-align: center;
+//   display: block;
+// }
 </style>
